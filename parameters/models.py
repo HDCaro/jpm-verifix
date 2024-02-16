9fa953cef25e88eb0b8b3a512fa4cd00b6a20a14
Hugo Daniel Caro <hcaro@kopiustech.com>
2024-02-16 02:16:25 -0300

    Make one-to-one relationship between Parameter and User

--- parameters/models.py
+++ parameters/models.py
@@ -11,7 +11,7 @@ class Parameter(models.Model):
     parameter4 = models.CharField(max_length=100, blank=True)
     parameter5 = models.CharField(max_length=100, blank=True)
     created = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return str(self.id) + ' ' + self.user.username + ' - parameter1: ' + self.parameter1 + "..."
